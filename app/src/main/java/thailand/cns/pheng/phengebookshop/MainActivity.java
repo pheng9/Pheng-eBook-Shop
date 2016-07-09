c8f33776ed6581992209190aa2ab5d0dcaa2f07e
Pheng <chanachai@addnet.co.th>
2016-07-09 09:48:49 +0700

    Start App

---
+++ app/src/main/java/thailand/cns/pheng/phengebookshop/MainActivity.java
@@ -0,0 +1,13 @@
+package thailand.cns.pheng.phengebookshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
